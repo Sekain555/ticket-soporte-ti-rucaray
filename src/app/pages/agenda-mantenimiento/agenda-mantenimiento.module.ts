30f21ba18dc72bf70e03f40f8ebcbd8ac037de9a
Sekain555 <116925273+Sekain555@users.noreply.github.com>
2026-04-23 08:45:00 -0400

    Feature/vista agenda dia semana (#16)
    * feat: vista de agenda por día/semana con filtro de período y navegación
    
    * feat: vista calendario en agenda de mantenciones

--- src/app/pages/agenda-mantenimiento/agenda-mantenimiento.module.ts
+++ src/app/pages/agenda-mantenimiento/agenda-mantenimiento.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { AgendaMantenimientoPageRoutingModule } from './agenda-mantenimiento-routing.module';
-
 import { AgendaMantenimientoPage } from './agenda-mantenimiento.page';
-
 import { ComponentsModule } from '../../components/components.module';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { ComponentsModule } from '../../components/components.module';
     FormsModule,
     IonicModule,
     AgendaMantenimientoPageRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
   ],
   declarations: [AgendaMantenimientoPage]
 })
